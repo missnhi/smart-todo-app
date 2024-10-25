3c0b856743cc7e181c75214fd18ae68c88217de3
missnhi <nhi.phan.ley@gmail.com>
2024-10-25 10:18:26 -0600

    database and error container

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- TODO: Change the user passwords to hashed passwords once bcrypt is installed
 INSERT INTO users (username, email, password)
 VALUES
-('test', 'test@tester.com', 'password'),
-('janedoe', 'jane@doe.com', 'password');
+('test', 'test@tester.com', '$2a$10$hgz0pHtw1uXAQiJp6sWUueKF/DNpbux7J3fZGUc7nWB96VBcIQ48u'),
+('plysnh', 'plysnh@gmail.com', '$2a$10$hgz0pHtw1uXAQiJp6sWUueKF/DNpbux7J3fZGUc7nWB96VBcIQ48u');
--- db/seeds/02_task_lists.sql
+++ db/seeds/02_task_lists.sql
@@ -1,6 +1,11 @@
 INSERT INTO task_lists (user_id, name, icon_url, description)
 VALUES
-(1, 'To Watch', './', 'description'),
-(1, 'To Read', './', 'description'),
-(1, 'To Buy', './', 'description'),
-(1, 'To Eat', './', 'description');
+(1, 'To Watch','watch.png', 'description'),
+(1, 'To Read', 'reading.png', 'description'),
+(1, 'To Buy', 'shopping.png', 'description'),
+(1, 'To Eat','utensils.png','description'),
+
+(2, 'To Watch','watch.png', 'description'),
+(2, 'To Read', 'reading.png', 'description'),
+(2, 'To Buy', 'shopping.png', 'description'),
+(2, 'To Eat','utensils.png','description');
